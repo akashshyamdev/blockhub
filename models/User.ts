88216427693db24d58457cd95bbe470513ec88c0
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-02 14:39:38 +0530

    feat: initialised next.js

---
+++ models/User.ts
@@ -0,0 +1,20 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", userSchema);
+
+export default User;
