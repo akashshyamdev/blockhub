ab174d18565ef8b7a6415d923da32b6975bcf959
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-06 15:55:34 +0530

    chore(next-auth): added provider wrapper to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
+import { Provider } from "next-auth/client";
 import "tailwindcss/tailwind.css";
 import Nav from "../layout/Nav";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider>
       <Nav />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
