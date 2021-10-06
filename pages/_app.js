2ac44ccca1c4b81577755b7ffd8b8836fbfaf371
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-06 15:42:34 +0530

    chore(_app.js): imported Nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "tailwindcss/tailwind.css";
+import Nav from "../layout/Nav";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Nav />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
