7af5339fcdba005d7bf814ec8c9dfb5f67d9440e
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-07 16:56:15 +0530

    style(signup): improved UI

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,26 +1,35 @@
+import Heading from "@components/Heading/Heading";
 import OAuth from "@components/OAuth/OAuth";
-import { signOut, useSession } from "next-auth/client";
+import { useSession } from "next-auth/client";
 import Head from "next/head";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 export default function Signup() {
-  const [session, loading] = useSession();
+  const router = useRouter();
+  const [session] = useSession();
+
+  useEffect(() => {
+    if (session) router.push("/");
+  }, [session]);
 
   return (
-    <main className={"bg-gray-50 h-full"}>
+    <main className={"bg-gray-50 h-full flex flex-row justify-center items-center py-32"}>
       <Head>
         <title>Signup | Blockhub</title>
       </Head>
 
-      <div>
-        {!session && <OAuth />}
+      <div className={"flex flex-col justify-center items-center bg-white w-5/12 px-14 py-8"}>
+        <Heading align={"center"} variant={"h1"}>
+          Welcome to Blockhub
+        </Heading>
 
-        {session && (
-          <>
-            Signed in as {session.user.name} <br />
-            <button onClick={() => signOut()}>Sign out</button>
-          </>
-        )}
+        <Heading align={"center"} variant={"h3"} className={"mt-5"}>
+          The place where blockchain enthusiasts come together, share their knowledge and meet
+          like-minded people
+        </Heading>
+
+        <OAuth containerClass={"py-16"} />
       </div>
     </main>
   );
