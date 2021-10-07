763755e32240c8f72b342363f63ed9f20b78de5d
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-07 09:25:03 +0530

    chore(.env): added FB client id and secret

--- global.d.ts
+++ global.d.ts
@@ -8,5 +8,11 @@ namespace NodeJS {
 
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
+
+    GITHUB_CLIENT_ID: string;
+    GITHUB_CLIENT_SECRET: string;
+
+    FACEBOOK_CLIENT_ID: string;
+    FACEBOOK_CLIENT_SECRET: string;
   }
 }
