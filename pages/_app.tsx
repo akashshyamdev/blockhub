f28e7da14c9423f4628fd1db19646bd81d79a1a8
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-07 14:49:06 +0530

    chore(Nav): moved into individual folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
+import Nav from "@layout/Nav/Nav";
 import { Provider } from "next-auth/client";
 import "tailwindcss/tailwind.css";
-import Nav from "../layout/Nav";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider>
-
       <Nav />
       <Component {...pageProps} />
     </Provider>
