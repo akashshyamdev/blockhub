5bdcded2cb8581364b5771c84ae195a7960a4c0d
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-07 18:53:48 +0530

    feat(Button): component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React, { MouseEventHandler } from "react";
+import classes from "./Button.module.scss";
+
+export interface ButtonProps {
+  href?: string;
+  children: string;
+  customClass?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function Button({ href, children, onClick, customClass }: ButtonProps) {
+  const className = `${customClass} ${classes.button}`;
+
+  return (
+    <>
+      {href ? (
+        <Link href={href}>
+          <a className={className}>{children}</a>
+        </Link>
+      ) : (
+        <button className={className} onClick={onClick}>
+          {children}
+        </button>
+      )}
+    </>
+  );
+}
