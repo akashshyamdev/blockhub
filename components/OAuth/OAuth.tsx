a59760a19fb134508001be0d11c67ad9f4d85b41
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-07 14:42:24 +0530

    feat(auth): twitter OAuth

--- components/OAuth/OAuth.tsx
+++ components/OAuth/OAuth.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function OAuth() {
   const googleAuth = () => signIn("google", { callbackUrl: "http://localhost:3000/" });
 
-  const githubAuth = () => signIn("github", { callbackUrl: "http://localhost:3000/" });
-
   const facebookAuth = () => signIn("facebook", { callbackUrl: "http://localhost:3000/" });
+
+  const twitterAuth = () => signIn("twitter", { callbackUrl: "http://localhost:3000/" });
 
   return (
     <div>
@@ -20,18 +20,18 @@ export default function OAuth() {
 
       <button
         className={"flex flex-row items-center px-7 py-5 border border-1 gap-x-3"}
-        onClick={githubAuth}
-      >
-        <Image src={"/assets/svg/github.svg"} width={"32"} height={"32"} />
-        Continue With Github
-      </button>
-
-      <button
-        className={"flex flex-row items-center px-7 py-5 border border-1 gap-x-3"}
         onClick={facebookAuth}
       >
         <Image src={"/assets/svg/facebook.svg"} width={"32"} height={"32"} />
         Continue With Facebook
+      </button>
+
+      <button
+        className={"flex flex-row items-center px-7 py-5 border border-1 gap-x-3"}
+        onClick={twitterAuth}
+      >
+        <Image src={"/assets/svg/twitter.svg"} width={"32"} height={"32"} />
+        Continue With Twitter
       </button>
     </div>
   );
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
-import { connectDB } from "../../../middleware/database";
-import User from "../../../models/User";
 
 const MONGODB_URI = process.env.DB_URL.replace("<user>", process.env.DB_USER).replace(
   "<password>",
@@ -21,20 +19,11 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    Providers.Credentials({
-      async authorize(credentials: { email: string; password: string }) {
-        await connectDB();
-
-        const user = await User.findOne({ email: credentials.email });
-        const isPasswordValid = await user.comparePasswords(credentials.password, user.password);
-
-        if (!user || !isPasswordValid) throw new Error("Email or password is incorrect!");
-
-        return { id: user._id };
-      },
+    Providers.Twitter({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
     }),
   ],
-
   database: MONGODB_URI,
   callbacks: {
     session: async (session, user) => {
@@ -43,3 +32,16 @@ export default NextAuth({
     },
   },
 });
+
+// Providers.Credentials({
+//   async authorize(credentials: { email: string; password: string }) {
+//     await connectDB();
+//
+//     const user = await User.findOne({ email: credentials.email });
+//     const isPasswordValid = await user.comparePasswords(credentials.password, user.password);
+//
+//     if (!user || !isPasswordValid) throw new Error("Email or password is incorrect!");
+//
+//     return { id: user._id };
+//   },
+// }),
