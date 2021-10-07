867012389e4a877e86e474156006cd4308a5a2dc
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-07 16:56:43 +0530

    chore(tailwind): updated fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ["Poppins", "sans-serif"],
+      sans: ["Open Sans", "sans-serif"],
+      serif: ["Gilda Display", "serif"]
+    },
     extend: {},
   },
   variants: {
