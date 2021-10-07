7bbd8f06da62894ff675317f42dba26a03436622
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-07 16:56:34 +0530

    feat(Heading): component

---
+++ components/Heading/Heading.tsx
@@ -0,0 +1,51 @@
+import React, { ReactNode } from "react";
+import classes from "./Heading.module.scss";
+
+export interface HeadingProps {
+  className?: string;
+  children: ReactNode;
+  align?: "left" | "center" | "right";
+  variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+}
+
+export default function Heading({ align = "left", children, className, variant }: HeadingProps) {
+  return (
+    <>
+      {variant === "h1" && (
+        <h1 className={`text-${align} ${classes.h1} ${classes.heading} ${className}`}>
+          {children}
+        </h1>
+      )}
+
+      {variant === "h2" && (
+        <h2 className={`text-${align} ${classes.h2} ${classes.heading} ${className}`}>
+          {children}
+        </h2>
+      )}
+
+      {variant === "h3" && (
+        <h3 className={`text-${align} ${classes.h3} ${classes.heading} ${className}`}>
+          {children}
+        </h3>
+      )}
+
+      {variant === "h4" && (
+        <h4 className={`text-${align} ${classes.h4} ${classes.heading} ${className}`}>
+          {children}
+        </h4>
+      )}
+
+      {variant === "h5" && (
+        <h5 className={`text-${align} ${classes.h5} ${classes.heading} ${className}`}>
+          {children}
+        </h5>
+      )}
+
+      {variant === "h6" && (
+        <h6 className={`text-${align} ${classes.h6} ${classes.heading} ${className}`}>
+          {children}
+        </h6>
+      )}
+    </>
+  );
+}
