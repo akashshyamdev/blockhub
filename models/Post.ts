051402dc9030d069ecb0ca0460c106c5a1291778
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-09 16:00:50 +0530

    feat(Post): model

---
+++ models/Post.ts
@@ -0,0 +1,27 @@
+import { IPost } from "@customTypes/post";
+import { Model, model, models, Schema, Types } from "mongoose";
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please specify a title"],
+    },
+    subTitle: {
+      type: String,
+      required: [true, "Please specify a sub title"],
+    },
+    user: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Post: Model<IPost> = models.Post || model<IPost>("Post", postSchema);
+
+export default Post;
