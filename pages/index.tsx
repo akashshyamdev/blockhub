a78f1843ab97d570d579e74ef95a9a59e09898b8
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-09 22:58:02 +0530

    feat(new post): basic md editor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
 export default function Home() {
+  const { data } = useSession();
+
+  console.log();
   return (
     <main>
       <h1>Content</h1>
+
+      <Link href={"/posts/create"}>
+        <a>Create Post</a>
+      </Link>
+
+      <button onClick={() => signOut()}>Sign out</button>
     </main>
   );
 }
