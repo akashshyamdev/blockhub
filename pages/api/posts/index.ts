54b7641e455ed3b3ec8760800ecee8fb56864ff9
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-10 12:51:52 +0530

    feat(posts): getAllPosts route

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -3,6 +3,20 @@ import Post from "@models/Post";
 import User from "@models/User";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const getAllPosts = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const posts = Post.find();
+
+    res.status(200).json({ data: posts });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      err,
+    });
+  }
+};
+
 const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { title, subTitle, email, content } = req.body;
@@ -10,8 +24,6 @@ const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
     const user = await User.findOne({ email });
 
     const post = await Post.create({ title, subTitle, content, user: user._id });
-
-    console.log(post);
 
     res.status(201).json({ data: post });
   } catch (err) {
@@ -26,7 +38,8 @@ const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await connectDB();
 
-  if (req.method === "POST") await createPost(req, res);
+  if (req.method === "GET") await getAllPosts(req, res);
+  else if (req.method === "POST") await createPost(req, res);
 
   res.status(404).json({ message: "There is no route here" });
 }
