b44a4b58e1a936ef7514dfda352a00a20b7a4432
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-10 16:15:29 +0530

    refactor(posts): simplified routes by using apiHandler wrapper

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,45 +1,27 @@
-import { connectDB } from "@middleware/mongoose";
 import Post from "@models/Post";
 import User from "@models/User";
+import apiHandler from "@utils/apiHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const getAllPosts = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const posts = Post.find();
-
-    res.status(200).json({ data: posts });
-  } catch (err) {
-    console.log(err);
-
-    res.status(500).json({
-      err,
-    });
-  }
+const getAllPosts = async () => {
+  return Post.find();
 };
 
 const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const { title, subTitle, email, content } = req.body;
+  const { title, subTitle, email, content } = req.body;
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    const post = await Post.create({ title, subTitle, content, user: user._id });
+  const post = await Post.create({ title, subTitle, content, user: user._id });
 
-    res.status(201).json({ data: post });
-  } catch (err) {
-    console.log(err);
-
-    res.status(500).json({
-      err,
-    });
-  }
+  res.status(201).json({ data: post });
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await connectDB();
+export default apiHandler({
+  get: async (req, res) => {
+    const posts = await getAllPosts();
 
-  if (req.method === "GET") await getAllPosts(req, res);
-  else if (req.method === "POST") await createPost(req, res);
-
-  res.status(404).json({ message: "There is no route here" });
-}
+    res.status(200).json({ data: posts });
+  },
+  post: createPost,
+});
