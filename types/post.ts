e67657e3e0dad853dfd71433f4aa8c0dbf0d5091
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-10 15:43:59 +0530

    types(post): added title, subTitle and content fields

--- types/post.ts
+++ types/post.ts
@@ -2,6 +2,9 @@ import { IUser } from "@customTypes/user";
 import { Document } from "mongoose";
 
 export interface IPost extends Document {
+  title: string;
+  subTitle: string;
+  content: string;
   name: string;
   email: string;
   user: IUser;
