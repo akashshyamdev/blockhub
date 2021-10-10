d4846277e82fc045ae65dc394d9c1e74419a869f
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-10 14:23:24 +0530

    refactor(index.tsx): removed redundant create post link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,7 @@
-import { signOut } from "next-auth/react";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <main>
       <h1>Content</h1>
-
-      <Link href={"/posts/create"}>
-        <a>Create Post</a>
-      </Link>
-
-      <button onClick={() => signOut()}>Sign out</button>
     </main>
   );
 }
