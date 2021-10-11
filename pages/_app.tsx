727da38b07f8ad998e953e7e754d0f0dccc88721
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-11 09:13:43 +0530

    feat(_app): initialised I18n

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
+import { I18n } from "@aws-amplify/core";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "@layout/Layout/Layout";
+import { strings } from "@lib/string";
 import "@styles/globals.scss";
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    I18n.putVocabularies(strings);
+  }, []);
+
   return (
     <SessionProvider>
       <ChakraProvider>
