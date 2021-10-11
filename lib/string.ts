882d5191d1ff297dc89044342153534f7804533f
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-11 09:12:08 +0530

    feat(new post): I18n

---
+++ lib/string.ts
@@ -0,0 +1,8 @@
+export const strings = {
+  en: {
+    createPost: "Create Post",
+    createPostTitlePlaceholder: "Title goes here...",
+    createPostSubTitlePlaceholder: "Sub Title goes here...",
+    createPostContentPlaceholder: "Markdown goes here...",
+  },
+};
