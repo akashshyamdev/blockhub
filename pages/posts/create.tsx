4aaaebf90f0d91e10ecd766dda3a371492c2b912
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-12 21:32:05 +0530

    feat(new post): added toast on create and save

--- pages/posts/create.tsx
+++ pages/posts/create.tsx
@@ -1,11 +1,13 @@
 import { I18n } from "@aws-amplify/core";
 import Button from "@components/Button/Button";
+import { IPost } from "@customTypes/post";
 import { uploadCoverImage } from "@lib/s3";
 import classes from "@styles/components/preview.module.scss";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import snarkdown from "snarkdown";
 
 function CreatePost() {
@@ -20,6 +22,17 @@ function CreatePost() {
   const [markdown, setMarkdown] = useState("");
   const [result, setResult] = useState("");
 
+  useEffect(() => {
+    const draft: IPost & { file: File } = JSON.parse(localStorage.getItem("draft"));
+
+    if (draft) {
+      setTitle(draft.title);
+      setSubTitle(draft.subTitle);
+      setMarkdown(draft.content);
+      setFile(draft.file);
+    }
+  }, []);
+
   const createPost = async () => {
     const res = await uploadCoverImage(file);
 
@@ -31,7 +44,19 @@ function CreatePost() {
       image: res.Location,
     });
 
+    localStorage.removeItem("draft");
+
+    toast.success("Published Post!");
+
     router.push("/");
+  };
+
+  const savePost = async () => {
+    localStorage.setItem("draft", JSON.stringify({ title, subTitle, content: markdown }));
+
+    toast.success("Saved Draft(except cover image)", {
+      onClose: () => console.log("A"),
+    });
   };
 
   const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
@@ -48,6 +73,7 @@ function CreatePost() {
   return (
     <main className={"flex flex-row flex-grow full justify-center py-10"}>
       <div className={"flex flex-col h-full w-8/12 items-center"}>
+        {/* Heading */}
         <div className={"w-full mb-10"}>
           <div className={"mb-3 cursor-pointer"}>
             <label htmlFor="fileInput" className={"border cursor-pointer p-5 rounded"}>
@@ -77,6 +103,7 @@ function CreatePost() {
           />
         </div>
 
+        {/* Markdown */}
         <div className={"grid w-full h-full grid-cols-2 mt-3"}>
           <div>
             <textarea
@@ -96,8 +123,13 @@ function CreatePost() {
           />
         </div>
 
+        {/* Buttons */}
         <div className={"mt-8 flex w-full flex-row justify-start"}>
           <Button onClick={createPost}>{I18n.get("createPost")}</Button>
+
+          <Button onClick={savePost} customClass={"ml-4"}>
+            Save Post
+          </Button>
         </div>
       </div>
     </main>
