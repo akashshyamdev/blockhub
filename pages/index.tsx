d1da5049fea072d4bf30dfdeff564ceb31271cc8
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-12 16:49:42 +0530

    feat(posts): added cover image preview

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { calculateReadingTime } from "@lib/reading";
 import classes from "@styles/components/post.module.scss";
 import axios from "axios";
 import { GetStaticProps } from "next";
+import Image from "next/image";
 import Link from "next/link";
 
 export interface HomeProps {
@@ -21,17 +22,23 @@ export default function Home({ posts }: HomeProps) {
           return (
             <Link href={{ pathname: `/posts/[slug]`, query: { slug: post.slug } }} key={post._id}>
               <article className={classes.post}>
-                <h3 className={classes.post__title}>{post.title}</h3>
-                <h5 className={classes.post__subtitle}>{post.subTitle}</h5>
+                <div>
+                  <h3 className={classes.post__title}>{post.title}</h3>
+                  <h5 className={classes.post__subtitle}>{post.subTitle}</h5>
 
-                <div className={classes.post__stats}>
-                  <p>{formatPostDate(post.createdAt)}</p>
-                  <div className={classes.post__separator}>&nbsp;</div>
+                  <div className={classes.post__stats}>
+                    <p>{formatPostDate(post.createdAt)}</p>
+                    <div className={classes.post__separator}>&nbsp;</div>
 
-                  <p>{time.readingTime} min(s)</p>
-                  <div className={classes.post__separator}>&nbsp;</div>
+                    <p>{time.readingTime} min(s)</p>
+                    <div className={classes.post__separator}>&nbsp;</div>
 
-                  <p>{time.wordCount} words</p>
+                    <p>{time.wordCount} words</p>
+                  </div>
+                </div>
+
+                <div className={classes.post__cover}>
+                  <Image src={post.coverImage} layout="fill" className={classes.post__coverImage} />
                 </div>
               </article>
             </Link>
