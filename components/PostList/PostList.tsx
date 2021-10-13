b1af23b95a80cfe7d08176062d2d2dc1b234383a
Akash Shyam <akash.shyam2008@gmail.com>
2021-10-13 19:22:40 +0530

    feat(PostList): component

---
+++ components/PostList/PostList.tsx
@@ -0,0 +1,45 @@
+import { IPost } from "@customTypes/post";
+import { formatPostDate } from "@lib/date";
+import { calculateReadingTime } from "@lib/reading";
+import classes from "@styles/components/post.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+export interface PostListProps {
+  posts: IPost[];
+}
+
+export default function PostList({ posts }: PostListProps) {
+  return (
+    <>
+      {posts?.map((post) => {
+        const time = calculateReadingTime(post);
+
+        return (
+          <Link href={{ pathname: `/posts/[slug]`, query: { slug: post.slug } }} key={post._id}>
+            <article className={classes.post}>
+              <div>
+                <h3 className={classes.post__title}>{post.title}</h3>
+                <h5 className={classes.post__subtitle}>{post.subTitle}</h5>
+
+                <div className={classes.post__stats}>
+                  <p>{formatPostDate(post.createdAt)}</p>
+                  <div className={classes.post__separator}>&nbsp;</div>
+
+                  <p>{time.readingTime} min(s)</p>
+                  <div className={classes.post__separator}>&nbsp;</div>
+
+                  <p>{time.wordCount} words</p>
+                </div>
+              </div>
+
+              <div className={classes.post__cover}>
+                <Image src={post.coverImage} layout="fill" className={classes.post__coverImage} />
+              </div>
+            </article>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
